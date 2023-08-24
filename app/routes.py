c444e021d7c2de048d7964bbc867a9c05cbe5086
Mariano <12319841+marianobonilla@users.noreply.github.com>
2023-08-23 17:43:19 -0700

    doc type enabled

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import os
-from .utils import extract_text_from_pdf
+from .utils import extract_text_from_pdf, extract_text_from_doc
 from app import app
 
 #app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'app/uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'pdf'}
+app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'docx'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
@@ -23,12 +23,17 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
-            text = extract_text_from_pdf(filepath)
-
+            file_extension = os.path.splitext(file.filename)
+            if file_extension == 'pdf':
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                text = extract_text_from_pdf(filepath)
+            else:
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                text = extract_text_from_doc(filepath)
             return render_template('results.html', extracted_text = text)
     
     return render_template('upload.html')
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,12 @@
 from pdfminer.high_level import extract_text
+from docx import Document
 
 def extract_text_from_pdf(pdf_path):
     return extract_text(pdf_path)
+
+def extract_text_from_doc(doc_path):
+    doc = Document(doc_path)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text + "\n"
+    return text
